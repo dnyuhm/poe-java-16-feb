72fb7794e0583bf81ec0e28906543278dc14b8aa
dnyuhm <dylanmeuriot@gmail.com>
2022-02-16 16:29:32 +0100

    Commit 1 : Modules

---
+++ src/app/page-not-found/page-not-found.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { PageNotFoundRoutingModule } from './page-not-found-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, PageNotFoundRoutingModule],
+})
+export class PageNotFoundModule {}
