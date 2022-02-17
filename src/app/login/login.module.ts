f9107f0d514a90fad28de3723cf23ee4494fe9bd
dnyuhm <dylanmeuriot@gmail.com>
2022-02-17 10:10:05 +0100

    Commit 3 : pages

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
+import { PageSignInComponent } from './pages/page-sign-in/page-sign-in.component';
+import { PageSignUpComponent } from './pages/page-sign-up/page-sign-up.component';
+import { PageResetPasswordComponent } from './pages/page-reset-password/page-reset-password.component';
+import { PageForgotPasswordComponent } from './pages/page-forgot-password/page-forgot-password.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PageSignInComponent,
+    PageSignUpComponent,
+    PageResetPasswordComponent,
+    PageForgotPasswordComponent
+  ],
   imports: [
     CommonModule,
     LoginRoutingModule
--- src/app/page-not-found/page-not-found.module.ts
+++ src/app/page-not-found/page-not-found.module.ts
@@ -1,9 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { PageNotFoundRoutingModule } from './page-not-found-routing.module';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PageNotFoundComponent
+  ],
   imports: [CommonModule, PageNotFoundRoutingModule],
 })
 export class PageNotFoundModule {}
