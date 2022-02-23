48616cfdfaead26058473d71ab4a2dc42f947bdb
dnyuhm <dylanmeuriot@gmail.com>
2022-02-23 15:24:14 +0100

    Commit 22 : Templates de page avec un title statique

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { IconsModule } from '../icons/icons.module';
+import { TemplatesModule } from '../templates/templates.module';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
+  exports: [TemplatesModule, IconsModule],
 })
 export class SharedModule {}
---
+++ src/app/templates/components/template-container/template-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-container',
+  templateUrl: './template-container.component.html',
+  styleUrls: ['./template-container.component.scss'],
+})
+export class TemplateContainerComponent implements OnInit {
+  @Input() public title: string;
+  constructor() {
+    this.title = 'Ohhhhh le joli title';
+  }
+
+  ngOnInit(): void {}
+}
