85b042094cb9cdfc2e4572a937db37882bfc52f1
dnyuhm <dylanmeuriot@gmail.com>
2022-02-23 14:23:13 +0100

    Commit 24 : Exercice “version” sur les services & observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  public title: string;
+  public title!: string;
+  private obs$: Observable<any>;
+  private subj$: Subject<any>;
+  private behave$: BehaviorSubject<any>;
 
   constructor() {
-    this.title = 'crm';
+    this.obs$ = new Observable<any>((listX) => {
+      listX.next(Math.random());
+    });
+    this.subj$ = new Subject<any>();
+    this.behave$ = new BehaviorSubject<any>('toto');
+
+    // this.obs$.subscribe((data) => console.log(data));
+    // this.obs$.subscribe((data) => console.log(data));
+    // this.subj$.subscribe((data) => console.log(data));
+    // this.subj$.next('toto');
+    // this.subj$.next('toto2');
+    // this.subj$.subscribe((data) => console.log(data));
+    // this.subj$.next(Math.random());
+    // this.behave$.next('toto2');
+    // this.behave$.subscribe((data) => console.log(data));
+    // this.behave$.next(Math.random());
+    // this.behave$.subscribe((data) => console.log(data));
   }
 }
