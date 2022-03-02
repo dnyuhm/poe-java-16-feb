c265eed4d75920b28384f44c09dcfaf06c70d5d2
dnyuhm <dylanmeuriot@gmail.com>
2022-03-02 14:54:11 +0100

    Commit étape 33 : FormBuilder and ReactiveFormsModule with FormControl and FormGroup

--- src/app/orders/components/form-order/form-order.component.ts
+++ src/app/orders/components/form-order/form-order.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { StateOrder } from 'src/app/core/enums/state-order';
 import { Order } from 'src/app/core/models/order';
 
@@ -8,12 +9,24 @@ import { Order } from 'src/app/core/models/order';
   styleUrls: ['./form-order.component.scss'],
 })
 export class FormOrderComponent implements OnInit {
-  public states: string[];
   @Input() public init!: Order;
+  public states: string[];
+  public form!: FormGroup;
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
     this.states = Object.values(StateOrder);
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      typePresta: [this.init.typePresta],
+      client: [this.init.client],
+      nbJours: [this.init.nbJours],
+      tjmHt: [this.init.tjmHt],
+      tva: [this.init.tva],
+      state: [this.init.state],
+      comment: [this.init.comment],
+      id: [this.init.id],
+    });
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { IconsModule } from '../icons/icons.module';
 import { TemplatesModule } from '../templates/templates.module';
@@ -10,6 +11,13 @@ import { TotalPipe } from './pipes/total.pipe';
 @NgModule({
   declarations: [TableLightComponent, BtnComponent, TotalPipe],
   imports: [CommonModule, RouterModule],
-  exports: [TemplatesModule, IconsModule, TableLightComponent, BtnComponent, TotalPipe],
+  exports: [
+    TemplatesModule,
+    IconsModule,
+    TableLightComponent,
+    BtnComponent,
+    TotalPipe,
+    ReactiveFormsModule,
+  ],
 })
 export class SharedModule {}
