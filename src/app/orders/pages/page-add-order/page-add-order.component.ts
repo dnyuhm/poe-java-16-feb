22dc18c80cb8874f2c097b1e94550199c0ea5056
dnyuhm <dylanmeuriot@gmail.com>
2022-03-02 14:29:12 +0100

    Commit 32 : Formulaire & addOrder

--- src/app/orders/pages/page-add-order/page-add-order.component.ts
+++ src/app/orders/pages/page-add-order/page-add-order.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/core/models/order';
 
 @Component({
   selector: 'app-page-add-order',
   templateUrl: './page-add-order.component.html',
-  styleUrls: ['./page-add-order.component.scss']
+  styleUrls: ['./page-add-order.component.scss'],
 })
 export class PageAddOrderComponent implements OnInit {
+  public order: Order;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.order = new Order();
   }
 
+  ngOnInit(): void {}
 }
