1693e93f89a2c8ba156b1c28eba0ca844e95ef02
dnyuhm <dylanmeuriot@gmail.com>
2022-03-02 15:43:59 +0100

    Commit étape 34: EventEmitter and Router.navigate()

--- src/app/orders/components/form-order/form-order.component.ts
+++ src/app/orders/components/form-order/form-order.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { StateOrder } from 'src/app/core/enums/state-order';
 import { Order } from 'src/app/core/models/order';
@@ -10,10 +10,12 @@ import { Order } from 'src/app/core/models/order';
 })
 export class FormOrderComponent implements OnInit {
   @Input() public init!: Order;
+  @Output() public submitted: EventEmitter<Order>;
   public states: string[];
   public form!: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {
+    this.submitted = new EventEmitter<Order>();
     this.states = Object.values(StateOrder);
   }
 
@@ -28,5 +30,9 @@ export class FormOrderComponent implements OnInit {
       comment: [this.init.comment],
       id: [this.init.id],
     });
+  }
+
+  public onSubmit(): void {
+    this.submitted.emit(this.form.value);
   }
 }
--- src/app/orders/pages/page-add-order/page-add-order.component.ts
+++ src/app/orders/pages/page-add-order/page-add-order.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Order } from 'src/app/core/models/order';
+import { OrdersService } from '../../services/orders.service';
 
 @Component({
   selector: 'app-page-add-order',
@@ -9,9 +11,15 @@ import { Order } from 'src/app/core/models/order';
 export class PageAddOrderComponent implements OnInit {
   public order: Order;
 
-  constructor() {
+  constructor(private ordersService: OrdersService, private router: Router) {
     this.order = new Order();
   }
 
   ngOnInit(): void {}
+
+  public action(item: Order): void {
+    this.ordersService
+      .add(item)
+      .subscribe(() => this.router.navigate(['orders']));
+  }
 }
