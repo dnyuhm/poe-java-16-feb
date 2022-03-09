f12b90e6897d4aae4ab7c0df01046739212d3202
dnyuhm <dylanmeuriot@gmail.com>
2022-03-09 09:04:54 +0100

    tp delete

--- src/app/orders/pages/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/pages/page-list-orders/page-list-orders.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { StateOrder } from 'src/app/core/enums/state-order';
 import { Order } from 'src/app/core/models/order';
 import { OrdersService } from '../../services/orders.service';
@@ -12,7 +12,7 @@ import { OrdersService } from '../../services/orders.service';
 })
 export class PageListOrdersComponent implements OnInit {
   public title: string;
-  public collection$: Observable<Order[]>;
+  public collection$: BehaviorSubject<Order[]>;
   public states: string[];
   public headers: string[];
 
@@ -45,6 +45,10 @@ export class PageListOrdersComponent implements OnInit {
 
   public goToEdit(id: number): void {
     this.router.navigate(['orders', 'edit', id]);
+  }
+
+  public deleteItem(id: number): void {
+    this.ordersService.delete(id).subscribe();
   }
 
   // Nous n'allons pas utiliser cette méthode dans le html car elle est appelée bien plus que necessaire
